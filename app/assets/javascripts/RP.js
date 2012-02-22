e1afa00048ab4fde1b5ebaee5263db13efd3b072
Steve Upstill <steve@upstill.net>
2012-02-22 15:58:13 +1300

    More lines in RP.js...

--- app/assets/javascripts/RP.js
+++ app/assets/javascripts/RP.js
@@ -1,4 +1,3 @@
-
 // GUARANTEED NECESSARY
 // Callback to respond to value changes on form elements,
 //  triggering immediate response
@@ -13,3 +12,39 @@ function queryownerChange() {
     queryheaderHit(this.form);
     queryformHit(this.form);
 }
+
+function queryListmodeChange() {
+    var form = $("form")[0];
+    var formstr = $(form).serialize();
+    // Add the popup to the rcpquery params in the form
+    var div = $('select#rcpquery_listmode_str')[0];
+    var divstr = $(div).serialize();
+    var datastr = "element=tabnum&" + formstr + "&" + divstr;
+    //...and proceed with the form hit as usual
+    var resp =
+    jQuery.ajax({
+        type: "POST",
+        url: form.action,
+        data: datastr,
+        // Submit the data from the form
+        dataType: "html",
+        success: queryresultsUpdate
+    }
+    );
+}
+
+// Handle a hit on one of the query fields by POSTing the whole form, then
+// (via callback) updating the results list
+function queryformHit(form) {
+    var resp =
+    jQuery.ajax({
+        type: "POST",
+        url: form.action,
+        data: "element=tabnum&" + $(form).serialize(),
+        // Submit the data from the form
+        dataType: "html",
+        success: queryresultsUpdate
+    }
+    );
+}
+
