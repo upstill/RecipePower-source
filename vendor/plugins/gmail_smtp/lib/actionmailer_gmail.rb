49dafffe4cf28a14e43db169ed129c6e8542dec7
Steve Upstill <steve@upstill.net>
2012-08-03 14:55:36 -0700

    First pass with Mailgun

--- vendor/plugins/gmail_smtp/lib/actionmailer_gmail.rb
+++ vendor/plugins/gmail_smtp/lib/actionmailer_gmail.rb
@@ -1,3 +1,4 @@
+=begin
 ActionMailer::Base.smtp_settings = {
 	:address => "smtp.gmail.com",
 	:port => 587,
@@ -6,3 +7,13 @@ ActionMailer::Base.smtp_settings = {
 	:user_name => ENV['GMAIL_SMTP_USER'],
 	:password => ENV['GMAIL_SMTP_PASSWORD'],
 }
+=end
+ActionMailer::Base.smtp_settings = {
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :port           => ENV['MAILGUN_SMTP_PORT'], 
+  :authentication => :plain,
+  :domain         => 'strong-galaxy-5765.heroku.com',
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+}
+ActionMailer::Base.delivery_method = :smtp
