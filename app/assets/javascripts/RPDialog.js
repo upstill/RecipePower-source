46e685574ff5653add0c57568c98ddfe132ef197
Steve Upstill <steve@upstill.net>
2012-10-01 10:22:06 -0700

    Made tags editor at left be positioned relative to window

--- app/assets/javascripts/RPDialog.js
+++ app/assets/javascripts/RPDialog.js
@@ -319,6 +319,7 @@ function injectDialog(code, area, modeless) {
 	// Forms submissions that expect JSON structured data will be handled here:
 	$('form.json', dlog).submit( dlog, submitDialogForJSON );
 	if(area == "at_left") {
+		$(dlog).css( "top", window.pageYOffset );
 		// Right-shift the injector to accommodate the editor
 		var dlgwidth = $(dlog).width();
 		var injectorwidth = $('#RecipePowerInjectedEncapsulation').width();
