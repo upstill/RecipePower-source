8795c0437f4cb02aeb73e735e430ce515dbec3bb
Steve Upstill <steve@upstill.net>
2012-12-13 20:18:25 +1300

    Getting collection assets to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ RP::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( injector.css injector.js )
+  config.assets.precompile += %w( collection.css injector.js injector.css injector.js )
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
