55bd8f832c0aac1d7d65745409408eeb58548a3f
Steve Upstill <steve@upstill.net>
2013-01-31 17:10:58 +1300

    Scraping pages for feeds works

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,11 +1,11 @@
 module FeedsHelper
   def feedlist
     @feed.items.collect { |item|
+      publish_date = item.published ? "Published on: "+item.published.strftime("%B %d, %Y")+"<br />" : ""
       %Q{
         <p><a href='#{item.url}'>#{item.title}
        </a><br />
-          Published on: #{item.published.strftime("%B %d, %Y")}
-       <br />
+       #{publish_date}
        #{item.summary}</p><hr>
       }
     }.join.html_safe
