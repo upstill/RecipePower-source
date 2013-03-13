d0f876941369aa377720a7b900aa978e247ce5eb
Steve Upstill <steve@upstill.net>
2013-03-13 18:34:25 +1300

    Recipe thumb url falls back to MissingPicture.png

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -156,7 +156,7 @@ class Recipe < ActiveRecord::Base
       self.thumbnail = nil
       picurl
     else 
-      (thumbnail && thumbnail.thumbdata) || picurl || ""
+      (thumbnail && thumbnail.thumbdata) || picurl || "MissingPicture.png"
     end
   end
 
