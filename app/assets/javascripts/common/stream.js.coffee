49cde886912f539441bd22d834e63ec5ac507c45
Steve Upstill <steve@upstill.net>
2013-10-19 17:41:02 +1300

    Works with masonry for display, but not buffering

--- app/assets/javascripts/common/stream.js.coffee
+++ app/assets/javascripts/common/stream.js.coffee
@@ -3,17 +3,22 @@ RP.stream ||= {}
 
 jQuery ->
 	# Set load handler to invoke stream and handle items
-	debugger
 	$('div.collection div.streamer').each RP.stream.go
 
 RP.stream.go = (index, elmt )->
 	source = new EventSource('/stream/stream')
 	source.addEventListener 'end_of_stream', (e) ->
 		source.close()
+	source.addEventListener 'collection_element', (e) ->
+		jdata = JSON.parse e.data
+		item = $(jdata.elmt)
+		$('#masonry-container').append item
+		$('#masonry-container').masonry 'appended', item
+		
+RP.stream.buffer_test = ->
+	source = new EventSource('/stream/buffer_test')
+	source.addEventListener 'end_of_stream', (e) ->
+		source.close()
 	source.addEventListener 'message', (e) ->
 		$('#masonry-container').append("<div>"+e.data+"</div>")
-	source.addEventListener 'collection_element', (e) ->
-		debugger
-		jdata = e.data # JSON.parse e.data
-		$('#masonry-container').append(jdata)
-		# $('#masonry-container').masonry( 'addItems', $('#masonry-container').append(jdata.elmt) );	
+
--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -6,6 +6,21 @@ class StreamController < ApplicationController
   
   def send_header
   end
+  
+  def buffer_test
+    response.headers["Content-Type"] = "text/event-stream"
+    response.stream.write "data: Here's an item\n\n"
+    sleep(5)
+    response.stream.write "data: Here's another item\n\n"
+    sleep(5)
+    response.stream.write "data: Here's a third item\n\n"
+  rescue IOError
+    debugger
+    logger.info "Stream closed"
+  ensure
+    response.stream.write "event: end_of_stream\ndata: null\n\n"
+    response.stream.close
+  end
 
   # Streams items in the current query
   def stream
@@ -14,20 +29,12 @@ class StreamController < ApplicationController
     # response.stream.write "data: <div>This is another result ##{n.to_s}</div>\n\n"
     setup_collection false
     n = 1
-    debugger
-    response.stream.write "data: Here's an item\n\n"
-    response.stream.write "data: Here's another item\n\n"
-    response.stream.write "data: Here's a third item\n\n"
-=begin
  	  @seeker.results_paged[0..5].each do |element| 
- 	    # itemstr = with_format("html") { render_to_string partial: "collection/element", locals: { element: element }}
- 	    # item = with_format("html") { view_context.collection_element element }
-      itemstr = "<div>nothing special</div>" # JSON.dump ({ elmt: item }) # element.id })
+      item = with_format("html") { view_context.collection_element element } # "<div>Just one element</div" # 
+      itemstr = JSON.dump ( { elmt: item } )
       n = n+1
-      debugger
-      response.stream.write "event: collection_element\ndata: #{itemstr}\n\n\n"
+      response.stream.write "event: collection_element\ndata: #{itemstr}\n\n"
  	  end
-=end
   rescue IOError
     debugger
     logger.info "Stream closed"
