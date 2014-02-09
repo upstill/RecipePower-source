e11a8a20bdd5ceffa0bb98ba883634d7badb6357
Steve Upstill <steve@upstill.net>
2014-02-10 12:33:36 +1300

    Now using polite_name for user issuing share

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -14,7 +14,7 @@ class InvitationsController < Devise::InvitationsController
     self.resource = resource_class.new()
     resource.shared_recipe = params[:recipe_id]
     @recipe = resource.shared_recipe && Recipe.find(resource.shared_recipe)
-    self.resource.invitation_issuer = current_user.fullname.blank? ? current_user.handle : current_user.fullname
+    self.resource.invitation_issuer = current_user.polite_name
     # dialog_boilerplate(@recipe ? :share : :new)
     if @recipe
       smartrender :action => :share
