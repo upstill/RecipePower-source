329c6ae6b0b339f2b742f6c131aa59b8599ffe9c
Steve Upstill <steve@upstill.net>
2014-02-20 00:16:24 +1300

    token_input_field now subsumes taggable_field, including in taggable_div (NB TESTING REQUIRED!!)

--- app/helpers/taggable_helper.rb
+++ app/helpers/taggable_helper.rb
@@ -1,5 +1,6 @@
 module TaggableHelper
   
+=begin
   # Declare a tagging field, either within the context of a form, or as a vanilla text field
   def taggable_field(f, options={})
     object = (f.class.to_s.match /FormBuilder/) ? f.object : f
@@ -13,6 +14,7 @@ module TaggableHelper
   		f.text_field :tag_tokens, options
 		end
   end
+=end
 
   # Generalization of taggable_field for arbitrary attribute names (not just 'tags'--the default), and allowing
   #   for both form_for and simple_form fields as well as for raw objects
@@ -20,17 +22,16 @@ module TaggableHelper
   #   1) tags can be set by 'tag_token='('tag_tokens=') -- this is the name of the field
   #   2) the text for the field can be read from and written to 'tagtxt' ('tagstxt')
 
-  def token_input_field f, tags_attribute_name, options={}
+  def token_input_field f, tags_attribute_name="tags", options={}
     if tags_attribute_name.is_a? Hash
       options = tags_attribute_name
-      tags_attribute_name = nil
+      tags_attribute_name = "tags"
     end
-    attribute_name = tags_attribute_name ? tags_attribute_name.to_s : "tags"
-    attribute_name_singular = attribute_name.singularize
-    is_plural = attribute_name_singular != attribute_name
-    tags_attribute_name = attribute_name_singular.clone
-    tags_attribute_name << "_tag" unless attribute_name_singular == "tag"
-    tags_input_field = attribute_name_singular+"_token"
+    aname = tags_attribute_name.to_s
+    tags_attribute_name = aname.singularize
+    is_plural = tags_attribute_name != aname
+    tags_input_field = tags_attribute_name+"_token"
+    tags_attribute_name << "_tag" unless tags_attribute_name == "tag"
     if is_plural
       tags_input_field << "s"
       tags_attribute_name << 's'
@@ -60,7 +61,7 @@ module TaggableHelper
     options[:rows] ||= "1"
     content_tag :div, 
       (label ? f.label(:tag_tokens, label) : "") + 
-      taggable_field( f, options), 
+      token_input_field( f, options),
       { class: classname+" tags" }, false
   end
   
