afc98112c008b623e47d745408380dff86403597
Steve Upstill <steve@upstill.net>
2014-02-24 17:09:49 +1300

    Fixed crash from getting tags with super_id

--- app/decorators/taggable_decorator.rb
+++ app/decorators/taggable_decorator.rb
@@ -11,7 +11,7 @@ class TaggableDecorator < Draper::Decorator
       else
         [Tag.typenum(tagtype)]
     end
-    strjoin (object.tags + object.tags(User.super_id)).uniq.select { |tag|
+    strjoin (object.tags + object.tags(owner_id: User.super_id)).uniq.select { |tag|
       matching_types.nil? || (matching_types.include? tag.tagtype)
     }.collect { |tag|
       h.link_to_modal tag.name, tag, class: "rcp_list_element_tag"
