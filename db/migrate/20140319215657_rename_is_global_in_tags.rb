e614a0458b615058f598041b5ae4af4dcc4a03f9
Steve Upstill <steve@upstill.net>
2014-03-21 17:02:12 +1300

    Removed now-redundant migration

--- db/migrate/20140319215657_rename_is_global_in_tags.rb
+++
@@ -1,8 +0,0 @@
-class RenameIsGlobalInTags < ActiveRecord::Migration
-  def up
-    rename_column :tags, :isGlobal, :is_global
-  end
-  def down
-    rename_column :tags, :is_global, :isGlobal
-  end
-end
