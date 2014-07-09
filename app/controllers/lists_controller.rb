2f2d4011c8c71ba0541733a2aef26a973a9d6f81
Steve Upstill <steve@upstill.net>
2014-07-08 18:08:47 -0700

    Editing and updating list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -21,6 +21,22 @@ class ListsController < ApplicationController
   end
 
   def update
+    @list = List.find params[:id]
+    if @list.update_attributes(params[:list])
+      respond_to do |format|
+        format.html { redirect_to lists_url, :status => :see_other, notice: "'#{list.name}' was successfully updated." }
+        format.json { render json: {
+            done: true,
+            replacements: [ [ "#list"+@list.id.to_s, with_format("html") { render_to_string partial: "lists/show_table_row" } ] ],
+            popup: "List saved" }
+        }
+      end
+    else
+      respond_to do |format|
+        format.html { render action: "edit" }
+        format.json { render json: @list.errors[:all], status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
@@ -29,6 +45,7 @@ class ListsController < ApplicationController
   def new
     puts "current_user: "+current_user.name
     @list = List.new(owner_id: current_user.id)
+    smartrender
   end
 
   def edit
