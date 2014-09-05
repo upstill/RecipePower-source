87ff0986a6a17387398a547e776a1a50dad8241d
Steve Upstill <steve@upstill.net>
2014-09-04 21:43:08 -0700

    Back to both unicorn and unicorn_rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
+# require "bundler/capistrano"  (rbates)
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'RecipePower'
+set :application, 'RP'
 set :repo_url, 'git@github.com:upstill/RecipePower-source.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/home/upstill/apps/RP'
+set :deploy_to, '/home/upstill/apps/#{application}'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -34,8 +35,43 @@ set :deploy_to, '/home/upstill/apps/RP'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
+after "deploy", "deploy:cleanup" # keep only the last 5 releases (rbates)
 
+namespace :deploy do
+  # Begin rbates
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+  # End rbates
+
+=begin
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
@@ -54,5 +90,6 @@ namespace :deploy do
       # end
     end
   end
+=end
 
 end
