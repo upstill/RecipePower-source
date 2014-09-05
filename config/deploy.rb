b6c4ffb990727b7896d9b890843fcb2d050e2219
Steve Upstill <steve@upstill.net>
2014-09-05 09:54:04 -0700

    Pushing deployment changes per capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,7 @@
 lock '3.2.1'
 
 set :application, 'RP'
+application = 'RP'
 set :repo_url, 'git@github.com:upstill/RecipePower-source.git'
 
 # Default branch is :master
@@ -40,31 +41,39 @@ namespace :deploy do
   # Begin rbates
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
+    task command do
+      on roles: :app, except: {no_release: true} do |host|
+        run "/etc/init.d/unicorn_#{application} #{command}"
+      end
     end
   end
 
-  task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-    run "mkdir -p #{shared_path}/config"
-    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-    puts "Now edit the config files in #{shared_path}."
+  task :setup_config do
+    on roles: :app do |task|
+      sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+      sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+      run "mkdir -p #{shared_path}/config"
+      put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+      puts "Now edit the config files in #{shared_path}."
+    end
   end
-  after "deploy:setup", "deploy:setup_config"
+  # after "deploy:setup", "deploy:setup_config"
 
-  task :symlink_config, roles: :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  task :symlink_config do
+    on roles: :app do
+      run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    end
   end
-  after "deploy:finalize_update", "deploy:symlink_config"
+  # after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote."
-  task :check_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-      puts "WARNING: HEAD is not the same as origin/master"
-      puts "Run `git push` to sync changes."
-      exit
+  task :check_revision do
+    on( :web ) do |host|
+      unless `git rev-parse HEAD` == `git rev-parse origin/staging`
+        puts "WARNING: HEAD is not the same as origin/staging"
+        puts "Run `git push` to sync changes."
+        exit
+      end
     end
   end
   before "deploy", "deploy:check_revision"
