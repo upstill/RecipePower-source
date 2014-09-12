71a39e7da49fa0aec50ee76aad770904c783825a
Steve Upstill <steve@upstill.net>
2014-09-12 12:04:55 -0700

    Getting Railscasts recipes to work with staging server

--- config/recipes/unicorn.rb
+++ config/recipes/unicorn.rb
@@ -9,7 +9,7 @@ namespace :unicorn do
   task :setup do # , roles: :app do
     on roles(:app) do
       execute "mkdir -p #{shared_path}/config"
-      template "unicorn.rb.erb", unicorn_config
+      template "unicorn.rb.erb", fetch(:unicorn_config)
       template "unicorn_init.erb", "/tmp/unicorn_init"
       execute "chmod +x /tmp/unicorn_init"
       sudo "mv /tmp/unicorn_init /etc/init.d/unicorn_#{fetch :application}"
