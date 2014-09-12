635a85167ae9f4c389e504094460bc4d5be876ff
Steve Upstill <steve@upstill.net>
2014-09-12 11:37:19 -0700

    Getting Railscasts recipes to work with staging server

--- config/recipes/nginx.rb
+++ config/recipes/nginx.rb
@@ -13,7 +13,7 @@ namespace :nginx do
   task :setup do
     on roles(:web) do
       template "nginx_unicorn.erb", "/tmp/nginx_conf"
-      execute sudo, "mv /tmp/nginx_conf /etc/nginx/sites-enabled/#{fetch :application}"
+      execute :mv, "/tmp/nginx_conf /etc/nginx/sites-enabled/#{fetch :application}"
       execute "#{sudo} rm -f /etc/nginx/sites-enabled/default"
       restart
     end
