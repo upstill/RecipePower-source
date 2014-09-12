7869b5088f7da40594f5852fc29031f9e5beef51
Steve Upstill <steve@upstill.net>
2014-09-12 12:58:34 -0700

    Getting Railscasts recipes to work with staging server

--- config/recipes/unicorn.rb
+++ config/recipes/unicorn.rb
@@ -1,8 +1,8 @@
-set(:unicorn_user) { fetch :user }
-set(:unicorn_pid) { "#{current_path}/tmp/pids/unicorn.pid" }
-set(:unicorn_config) { "#{shared_path}/config/unicorn.rb" }
-set(:unicorn_log) { "#{shared_path}/log/unicorn.log" }
-set(:unicorn_workers, 2)
+set :unicorn_user, fetch :user
+set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+set :unicorn_config, "#{shared_path}/config/unicorn.rb"
+set :unicorn_log, "#{shared_path}/log/unicorn.log"
+set :unicorn_workers, 2
 
 namespace :unicorn do
   desc "Setup Unicorn initializer and app configuration"
