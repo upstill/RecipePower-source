4afa7d93cde0b50a762d7e6503aece15fce1801f
Steve Upstill <steve@upstill.net>
2014-09-12 10:47:44 -0700

    Getting Railscasts recipes to work with staging server

--- config/recipes/check.rb
+++ config/recipes/check.rb
@@ -2,8 +2,8 @@ namespace :check do
   desc "Make sure local git is in sync with remote."
   task :revision do
     on roles(:web) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/#{branch}`
-        puts "WARNING: HEAD is not the same as origin/#{branch}"
+      unless `git rev-parse HEAD` == `git rev-parse origin/#{fetch :branch}`
+        puts "WARNING: HEAD is not the same as origin/#{fetch :branch}"
         puts "Run `git push` to sync changes."
         exit
       end
