040df86ae2648af23585029f67487936ad54857a
Steve Upstill <steve@upstill.net>
2014-09-15 15:49:36 -0700

    Trimming files not needed b/c of templates

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -14,7 +14,8 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '173.255.245.80', user: 'upstill', roles: %w{web app db}, primary: true
+server '192.155.84.190', user: 'upstill', roles: %w{web app db}, primary: true
+# server '173.255.245.80', user: 'upstill', roles: %w{web app db}, primary: true
 
 # Custom SSH Options
 # ==================
