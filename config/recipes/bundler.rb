825abd2cb2e842e98c30e38a4950b928029b6e85
Steve Upstill <steve@upstill.net>
2014-09-16 14:00:00 -0700

    Committing for running deploy

--- config/recipes/bundler.rb
+++ config/recipes/bundler.rb
@@ -5,5 +5,5 @@ namespace :bundler do
       sudo "apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties"
     end
   end
-  before "bundler:install", "bundler:preinstall"
+  after "deploy:updated", "bundler:preinstall"
 end
