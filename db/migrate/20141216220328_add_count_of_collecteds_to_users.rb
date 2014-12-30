a1de07fa043fb8df90829b66476d8b3855e8615f
Steve Upstill <steve@upstill.net>
2014-12-30 08:38:34 -0800

    Elided initialization of collection count for users

--- db/migrate/20141216220328_add_count_of_collecteds_to_users.rb
+++ db/migrate/20141216220328_add_count_of_collecteds_to_users.rb
@@ -1,7 +1,7 @@
 class AddCountOfCollectedsToUsers < ActiveRecord::Migration
   def up
     add_column :users, :count_of_collecteds, :integer, :null => false, :default => 0
-    Rcpref.counter_culture_fix_counts
+    # Rcpref.counter_culture_fix_counts
   end
 
   def down
