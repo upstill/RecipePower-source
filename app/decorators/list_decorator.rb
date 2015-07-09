0c0cf34b26e756046c17f702ea6710cf294def29
Steve Upstill <steve@upstill.net>
2015-07-09 16:57:42 -0700

    Defined imgdata  for List decorator which falls back to image from one of its members

--- app/decorators/list_decorator.rb
+++ app/decorators/list_decorator.rb
@@ -40,6 +40,21 @@ class ListDecorator < CollectibleDecorator
     super
   end
 
+  def imgdata use_fallback=false
+    if img = @object.imgdata
+      return img
+    elsif use_fallback
+      # The default fallback is to use an image from a member of the list
+      sco = ListServices.new(@object).tagging_scope @object.collectible_user_id # @userid
+      sco.each do |tagging|
+        if img = tagging.entity.imgdata
+          return img
+        end
+      end
+      @object.imgdata(true)
+    end
+  end
+
   # Define presentation-specific methods here. Helpers are accessed through
   # `helpers` (aka `h`). You can override attributes, for example:
   #
