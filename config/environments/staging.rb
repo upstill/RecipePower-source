4364ef2e04b6d72b8a686193c9280cf9c5fe4dbc
Steve Upstill <steve@upstill.net>
2015-07-14 17:56:38 -0700

    Added bootstrap_preface.css and jquery2.js to precompilation manifest

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -25,7 +25,7 @@ RP::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( collection.css collection.js injector.css injector.js )
+  config.assets.precompile += %w( jquery2 bootstrap_preface.css collection.css collection.js injector.css injector.js )
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
