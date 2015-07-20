4ea3a404edee1c09ccef13c6ba585e7a46bfaf72
Steve Upstill <steve@upstill.net>
2015-07-20 15:49:26 -0700

    Rephrased User#collection title; set up pop-cardlet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -96,12 +96,14 @@ class UsersController < CollectibleController
   # Show the user's entire collection
   def collection
     update_and_decorate # @user = User.find params[:id]
-	  if (@user.id == current_user_or_guest_id)
-      response_service.title = "My Whole Collection"
+    entity_type = params[:entity_type]
+    entity_type = entity_type ? entity_type.capitalize : "Collection"
+    if (@user.id == current_user_or_guest_id)
+      response_service.title = "My #{entity_type}"
       @empty_msg = "Nothing here yet. Click the Suggest button above to get some ideas.".html_safe
       @active_menu = :collections
     else
-      response_service.title = "#{@user.handle}'s Collection"
+      response_service.title = "#{@user.handle}'s #{entity_type}"
       @empty_msg = "They haven't collected anything?!? Why not Share something with them?"
       @active_menu = :friends
     end
