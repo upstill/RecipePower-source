94de842a2d963e6fe72dea26b71e0171dc2e2ca5
Steve Upstill <steve@upstill.net>
2015-10-09 17:57:26 -0400

    Converted list_decorator to single quotes, fixed bug in imgdata when no elmts of the list have a picture

--- app/decorators/list_decorator.rb
+++ app/decorators/list_decorator.rb
@@ -4,29 +4,29 @@ class ListDecorator < CollectibleDecorator
   delegate_all
 
   def sourcename
-    ""
+    ''
   end
 
   def sourcehome
-    ""
+    ''
   end
 
   def url
-    ""
+    ''
   end
 
   def description fallback=false
     @object.description.or_fallback {
       @object.name_tag.present? ?
       case @object.name_tag.name
-        when "Keepers"
-          "Recipes that are clearly worthwhile."
-        when "Favorites"
-          "The best in my collection."
-        when "Now Cooking"
-          "Recipes in active rotation."
-        when "To Try"
-          "Earmarked for later."
+        when 'Keepers'
+          'Recipes that are clearly worthwhile.'
+        when 'Favorites'
+          'The best in my collection.'
+        when 'Now Cooking'
+          'Recipes in active rotation.'
+        when 'To Try'
+          'Earmarked for later.'
       end : "Bogus list ##{@object.id}"
     }
   end
@@ -37,24 +37,19 @@ class ListDecorator < CollectibleDecorator
 
   def extract fieldname
     case fieldname.downcase
-      when "url"
-        ""
+      when 'url'
+        ''
     end
     super
   end
 
   def imgdata
-    if (img = @object.imgdata).present?
-      return img
-    else
+    if (img = @object.imgdata).blank?
       # The default fallback is to use an image from a member of the list
       sco = ListServices.new(@object).tagging_scope @object.collectible_user_id # @userid
-      sco.each do |tagging|
-        if (img = tagging.entity.imgdata).present?
-          return img
-        end
-      end
+      sco.each { |tagging| (img = tagging.entity.imgdata).present? }
     end
+    img if img.present?
   end
 
   # Define presentation-specific methods here. Helpers are accessed through
