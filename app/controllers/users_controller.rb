11f6a87260170a8325ea5f2af0aa70b410eb818e
Steve Upstill <steve@upstill.net>
2016-05-06 15:06:12 +1200

    Trying to get staging app deployed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < CollectibleController
   def index
     # 'index' page may be calling itself with filter parameters in the name and tagtype
     @select = params[:select]
-    response_service.title = (@select=="followees") ? "Friends" : "People"
+    response_service.title = (@select=='followees') ? 'Friends' : 'People'
     smartrender 
   end
 
@@ -88,7 +88,7 @@ class UsersController < CollectibleController
     @active_menu = params[:id].to_s == current_user_or_guest_id ? :home : :collections
     update_and_decorate 
     @empty_msg = "As you check out things in RecipePower, they will be remembered here."
-    response_service.title = "Recently Viewed"
+    response_service.title = 'Recently Viewed'
     smartrender 
   end
 
@@ -96,13 +96,13 @@ class UsersController < CollectibleController
   def collection
     update_and_decorate 
     label = (params[:result_type] || 'recipe').pluralize
-    if (response_service.user.id == current_user_or_guest_id)
+    if response_service.user.id == current_user_or_guest_id
       response_service.title = "My #{label}"
-      @empty_msg = "Nothing here yet. Click the Suggest button above to get some ideas.".html_safe
+      @empty_msg = 'Nothing here yet. Click the Suggest button above to get some ideas.'.html_safe
       @active_menu = :collections
     else
       response_service.title = "#{response_service.user.handle}'s #{label}"
-      @empty_msg = "They haven't collected anything?!? Why not Share something with them?"
+      @empty_msg = 'They haven\'t collected anything?!? Why not Share something with them?'
       @active_menu = :friends
     end
     smartrender 
@@ -112,12 +112,12 @@ class UsersController < CollectibleController
   def biglist
     update_and_decorate 
     @active_menu = :collections
-    response_service.title = "The Big List"
+    response_service.title = 'The Big List'
     smartrender 
   end
 
   def not_found
-    redirect_to root_path, :notice => "User not found", method: "get"
+    redirect_to root_path, :notice => 'User not found', method: 'get'
   end
 
   # With devise handling user creation, the only way we get here is from the 'identify' page.
@@ -131,14 +131,14 @@ class UsersController < CollectibleController
     begin
       followee = User.find(params[:id])
     rescue Exception => e
-      flash[:error] = "Couldn't find followee "+params[:id].to_s
+      flash[:error] = 'Couldn\'t find followee '+params[:id].to_s
     end
     if current_user && followee
       current_user.followees.delete followee
       current_user.save
-      flash[:notice] = "There you go! No longer following "+followee.handle
+      flash[:notice] = 'There you go! No longer following '+followee.handle
     else
-      flash[:error] ||= ": No current user"
+      flash[:error] ||= ': No current user'
     end
     redirect_to root_path
   end
@@ -146,10 +146,10 @@ class UsersController < CollectibleController
   def edit
     update_and_decorate
     @authentications = response_service.user.authentications if @decorator.id
-    @section = params[:section] || "profile"
-    response_service.title = "Edit Profile"
+    @section = params[:section] || 'profile'
+    response_service.title = 'Edit Profile'
     if [ :about ].include? @section.to_sym
-      render "edit_aspect", locals: { aspect: @section.to_sym }
+      render 'edit_aspect', locals: { aspect: @section.to_sym }
     else
       smartrender
     end
@@ -168,8 +168,8 @@ class UsersController < CollectibleController
     params[:id] = current_user.id if current_user
     update_and_decorate
     @authentications = response_service.user.authentications if @decorator.id
-    @section = params[:section] || "profile"
-    response_service.title = "My "+@section.capitalize
+    @section = params[:section] || 'profile'
+    response_service.title = 'My '+@section.capitalize
     smartrender action: 'edit'
   end
 
@@ -184,7 +184,7 @@ class UsersController < CollectibleController
       else
         response_service.title = 'Cookmarks from Update'
         flash[:message] = (response_service.user == current_user ? 'Your profile' : response_service.user.handle+"'s profile")+' has been updated.'
-        # redirect_to action: "collection"
+        # redirect_to action: 'collection'
         render 'collectible/update'
       end
     else
@@ -197,20 +197,20 @@ class UsersController < CollectibleController
   def sendmail
     if update_and_decorate
       case request.method
-      when "PATCH", "POST" # "GET" request opens the dialog, "PATCH" submits it
-        if response_service.user.mail_subject.blank? && !(params[:confirmed] && (params[:confirmed] == "1"))
+      when 'PATCH', 'POST' # 'GET' request opens the dialog, 'PATCH' submits it
+        if response_service.user.mail_subject.blank? && !(params[:confirmed] && (params[:confirmed] == '1'))
           # Go again
-          flash[:alert] = "No subject?!? If that's really what you want, send again."
-          @confirmed = "1"
+          flash[:alert] = 'No subject?!? If that\'s really what you want, send again.'
+          @confirmed = '1'
         elsif response_service.user.mail_body.blank?
-          flash.now[:error] = "No message?!? Don't want to bother someone with an empty email!"
+          flash.now[:error] = 'No message?!? Don\'t want to bother someone with an empty email!'
           render :errors
         else
           RpMailer.user_to_user(current_user, response_service.user).deliver_now
-          flash[:popup] = "Mail is on its way!"
+          flash[:popup] = 'Mail is on its way!'
           render :done
         end
-      when "GET"
+      when 'GET'
         smartrender
       end
     end
