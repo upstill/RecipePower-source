80c21cef76ebd440ee029666a9360e00203af273
Steve Upstill <steve@upstill.net>
2017-03-02 11:24:08 +1300

    Moved generic #destroy controller action into ApplicationController from CollectibleController; armed PageRefDecorator to do homelink with destroy button

--- app/decorators/page_ref_decorator.rb
+++ app/decorators/page_ref_decorator.rb
@@ -1,15 +1,24 @@
 class PageRefDecorator < Draper::Decorator
+  include Templateer
   delegate_all
 
   def name
     # The name by which the reference is referred to is either
     # 1) the link_text of the reference (preferred), or
     # 2) the name of the canonical expression of the reference's first referent
-    if title.present?
-      title
-    elsif referents.first
-      referents.first.name
+    if object.title.present?
+      object.title
+    elsif object.is_a?(Referrable) && object.referents.first
+      object.referents.first.name
     end
+  end
+
+  def title
+    name || url
+  end
+
+  def external_link
+    url
   end
 
 end
