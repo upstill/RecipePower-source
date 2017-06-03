b87697de425adbd11a23cc3cfb61c364513e45e4
Steve Upstill <steve@upstill.net>
2017-06-03 15:43:31 -0700

    Configuring 'domain' in session store to be specific

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,18 @@
 # Be sure to restart your server when you modify this file.
 
-RP::Application.config.session_store :cookie_store, key: '_rp_session', :domain => :all
+domain =
+    case
+      when Rails.env.production?
+        'www.recipepower.com'
+      when Rails.env.staging?
+        'staging.recipepower.com'
+      else
+        'local.recipepower.com'
+    end
+RP::Application.config.session_store :cookie_store,
+                                     key: '_rp_session',
+                                     :domain => domain,
+                                     :expire_after => 7.days
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
