a09b548a72cd6a200c09797707990e1fc3be9efb
Steve Upstill <steve@upstill.net>
2017-07-05 13:55:01 -0700

    Catching up with status migration

---
+++ db/migrate/20170630030721_change_status_for_site.rb
@@ -0,0 +1,12 @@
+class ChangeStatusForSite < ActiveRecord::Migration
+  def up
+	change_column :sites, :status, :integer, default: 0
+	change_column :recipes, :status, :integer, default: 0
+	Recipe.where(status: [0, nil]).each { |recipe| recipe.update_attribute(:status, 3) }
+	Site.where(status: [0, nil]).each { |site| site.update_attribute(:status, 3) }
+  end
+  def down
+	change_column :sites, :status, :integer
+	change_column :recipes, :status, :integer
+  end
+end
