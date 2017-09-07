afbb03da4d421323e8a1e95c942decbeaa26377c
Steve Upstill <steve@upstill.net>
2017-09-07 13:25:16 -0700

    submit_form_for working on user main page (outside dialog)

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -10,16 +10,18 @@ module FormsHelper
     form_for resource, merge_submit_options(options) do |f| block.call f end
   end
 
-  # Override of simple_form_for which adds options to make RP.submit happy
+  # Override of simple_form_for which asserts options to make RP.submit happy
   def simple_submit_form_for resource, options=nil, &block
-    simple_form_for resource, merge_submit_options(options) do |f| block.call f end
+    options = merge_submit_options options
+    options[:html][:data] = (options[:html][:data] || {}).merge options.delete(:data)
+    options[:html][:remote] = options.delete :remote
+    simple_form_for resource, options do |f| block.call f end
   end
 
 private
   # Define options for RP.submit to function properly
   def merge_submit_options options
     options = options ? options.deep_dup : {}
-    # form_for answer, remote: true, html: { onload: "RP.submit.form_onload(event);" }, data: {type: 'json'}
     (options[:data] ||= {})[:type] = 'json'
     (options[:html] ||= {})[:onload] = 'RP.submit.form_onload(event);'
     options[:html].delete 'data-type'
