77d29b732529724a2bcd75f8d15e5b213d1b060d
Steve Upstill <steve@upstill.net>
2018-10-05 17:24:24 -0400

    RefereeServices#assert_kind optionally promotes PageRef to Recipe or Site when those kinds are requested.

--- app/services/referee_services.rb
+++ app/services/referee_services.rb
@@ -7,11 +7,11 @@ class RefereeServices
 
   # Ensure a Pagerefable referee matches the kind of its page ref
   # Return the original object (if the kind matches) or a new, matching object
-  def assert_kind kind
+  def assert_kind kind, promote=false
     page_ref =
     case @referee
       when PageRef
-        return @referee if @referee.kind == kind
+        return @referee if @referee.kind == kind && !promote
         @referee
       when Pagerefable # Recipe, Site
         @referee.page_ref
@@ -23,8 +23,10 @@ class RefereeServices
       when 'recipe'
         # We want a recipe, but the current referee is not a recipe
         # Either the page_ref will have an associated recipe, or we make one
+        return @referee unless promote
         (@referee if @referee.is_a?(Recipe)) || page_ref.recipes.first || Recipe.new(page_ref: page_ref)
       when 'site'
+        return @referee unless promote
         (@referee if @referee.is_a?(Site)) || page_ref.sites.first || Site.new(page_ref: page_ref)
       else
         if page_ref.kind != kind # All other kinds are simply a type of PageRef. We convert and return
