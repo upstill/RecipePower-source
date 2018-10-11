47e350e0eb52f8bc3f41ee9d1595b4958fa2d6bf
Steve Upstill <steve@upstill.net>
2018-10-11 14:07:26 -0700

    Repaired failure in dealing with to Referent subclass in Edition

--- app/helpers/editions_helper.rb
+++ app/helpers/editions_helper.rb
@@ -2,9 +2,10 @@ module EditionsHelper
 
   def show_edition_item item, headline, before_text, after_text
     if item
+      item = item.becomes item.class.base_class
       decorator = item.decorate
       touch_params = { user_id: @touch_id }
-      touch_params[:redirect_external] = true if [Recipe, Site, FeedEntry].include? item.class.base_class
+      touch_params[:redirect_external] = true if [Recipe, Site, FeedEntry].include? item
       render 'editions/message_item',
              headline: headline,
              decorator: decorator,
