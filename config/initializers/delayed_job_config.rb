1f19cc233723e47124edc7baf225e56c9dffd328
Steve Upstill <steve@upstill.net>
2018-10-17 16:00:23 -0700

    Attempting to get DelayedJob to properly load models

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -9,3 +9,4 @@ Delayed::Worker.max_attempts = 3
 # Delayed::Worker.delay_jobs = !Rails.env.test?
 # Delayed::Worker.raise_signal_exceptions = :term
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
+require_relative '../../app/models/scraper.rb'
