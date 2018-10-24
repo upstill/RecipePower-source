652fa912339cc10914c22b96c2f9709ab0f6859d
Steve Upstill <steve@upstill.net>
2018-10-24 12:09:01 -0700

    Removed redundant migration on Editions

--- db/migrate/20181024175020_add_status_to_editions.rb
+++ db/migrate/20181024175020_add_status_to_editions.rb
@@ -1,6 +1,6 @@
 class AddStatusToEditions < ActiveRecord::Migration
   def change
-    add_column :editions, :status, :integer, default: 0
-    add_column :editions, :dj_id, :integer
+    # add_column :editions, :status, :integer, default: 0
+    # add_column :editions, :dj_id, :integer
   end
 end
