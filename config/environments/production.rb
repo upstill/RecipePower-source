9eecc2826146296839bb6aaeddd107e449b6c478
Steve Upstill <steve@upstill.net>
2019-03-13 16:14:14 +1300

    Replaced deprecated config.serve_static_files with config.public_file_server.enabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ RP::Application.configure do
   config.consider_all_requests_local       = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_files = true
+  config.public_file_server.enabled = true # config.serve_static_files = true
 
   # Compress JavaScripts and CSS
   config.assets.js_compressor = Uglifier.new(output: {
