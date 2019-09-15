0cae836d4471bb3d9bbd57df23ed3105d603fb54
Steve Upstill <steve@upstill.net>
2019-09-15 10:32:51 -0700

    defined #show in Gleanings controller

--- app/controllers/gleanings_controller.rb
+++ app/controllers/gleanings_controller.rb
@@ -1,2 +1,14 @@
 class GleaningsController < ApplicationController
+  before_action :set_gleaning, only: [:show, :edit, :update, :destroy]
+
+  def show
+
+  end
+
+  def set_gleaning
+    @recipe = Recipe.find_by params.slice(:id)
+    @page_ref = @recipe.page_ref
+    @page_ref.perform unless @page_ref.gleaning
+    @gleaning = @page_ref.gleaning
+  end
 end
