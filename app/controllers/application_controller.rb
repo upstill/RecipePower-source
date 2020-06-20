db9781e76c4dbda2a5ec92a0ce3de5d44cc649d0
Steve Upstill <steve@upstill.net>
2020-06-20 12:01:18 +1200

    Trying forcing SSL in production to fix Max's cookie problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -264,7 +264,7 @@ class ApplicationController < ActionController::Base
   end
 
   def report_headers h, label, which=nil
-    which ||= (label == 'Request header') ? %w{ HTTP_COOKIE rack.request.cookie_hash warden } : h.to_h.keys
+    which ||= h.to_h.keys # (label == 'Request header') ? %w{ HTTP_COOKIE rack.request.cookie_hash warden } : h.to_h.keys
     which -= [ 'async.callback' ] # Don't dump the callback object, no matter what
     which.each do |k|
       logger.info "#{label}: #{k}: ----------------"
