271171deeb35d45861bc886e94c51eab56531dc4
Steve Upstill <steve@upstill.net>
2020-07-06 14:11:28 -0700

    testing change

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,3 +7,24 @@
 
 worker_processes 1
 timeout ((ENV['RAILS_ENV'] == 'development') ? 3000 : 200)
+
+# set path to the application
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
+working_directory app_dir
+
+# Set unicorn options
+worker_processes 2
+preload_app true
+timeout 30
+
+# Path for the Unicorn socket
+listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+
+# Set path for logging
+stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+
+# Set proccess id path
+pid "#{shared_dir}/pids/unicorn.pid"
+
