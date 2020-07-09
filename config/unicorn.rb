31c6b232a60e176342b3b5227279f40c4088a0e2
Steve Upstill <steve@upstill.net>
2020-07-09 02:52:02 -0700

    Committing changes to unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,8 +3,6 @@
 # stderr_path "/var/www/RP/shared/log/unicorn.log"
 # stdout_path "/var/www/RP/shared/log/unicorn.log"
 
-# listen "/tmp/unicorn.RP.sock"
-
 # worker_processes 1
 timeout ((ENV['RAILS_ENV'] == 'development') ? 3000 : 200)
 
