9e649f31bab23d5a1c97fc5b46dd541d05804b2d
Steve Upstill <steve@upstill.net>
2020-07-23 10:01:33 -0700

    First pass at ironing out deployment methodology

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,20 +1,12 @@
-# working_directory "/var/www/RP/current"
-# pid "/var/www/RP/current/tmp/pids/unicorn.pid"
-# stderr_path "/var/www/RP/shared/log/unicorn.log"
-# stdout_path "/var/www/RP/shared/log/unicorn.log"
-
-# worker_processes 1
-timeout ((ENV['RAILS_ENV'] == 'development') ? 3000 : 200)
-
 # set path to the application
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 working_directory app_dir
 
 # Set unicorn options
-# worker_processes 2
+worker_processes 1
 preload_app true
-timeout 30
+timeout ((ENV['RAILS_ENV'] == 'development') ? 3000 : 200)
 
 # Path for the Unicorn socket
 listen "/var/sockets/unicorn.sock", :backlog => 64
