18bc5c8ae40252464cfb81049e08c7f8272ac06e
Steve Upstill <steve@upstill.net>
2020-11-18 18:19:44 -0800

    Subclassed ReferentPolicy so that its policies apply to subclasses of Referent

--- app/policies/referent_policy.rb
+++ app/policies/referent_policy.rb
@@ -68,5 +68,70 @@ class ReferentPolicy < CollectiblePolicy
 
 end
 
+class SourceReferentPolicy < ReferentPolicy
+end
+
+class InterestReferentPolicy < ReferentPolicy
+end
+
+class GenreReferentPolicy < ReferentPolicy
+end
+
+class RoleReferentPolicy < ReferentPolicy
+end
+
+class DishReferentPolicy < ReferentPolicy
+end
+
+class CourseReferentPolicy < ReferentPolicy
+end
+
+class ProcessReferentPolicy < ReferentPolicy
+end
+
+class IngredientReferentPolicy < ReferentPolicy
+end
+
+class UnitReferentPolicy < ReferentPolicy
+end
+
+class AuthorReferentPolicy < ReferentPolicy
+end
+
+class OccasionReferentPolicy < ReferentPolicy
+end
+
+class PantrySectionReferentPolicy < ReferentPolicy
+end
+
+class StoreSectionReferentPolicy < ReferentPolicy
+end
+
+class DietReferentPolicy < ReferentPolicy
+end
+
+class ToolReferentPolicy < ReferentPolicy
+end
+
+class NutrientReferentPolicy < ReferentPolicy
+end
+
+class CulinaryTermReferentPolicy < ReferentPolicy
+end
+
+class QuestionReferentPolicy < ReferentPolicy
+end
+
+class ListReferentPolicy < ReferentPolicy
+end
+
+class EpitaphReferentPolicy < ReferentPolicy
+end
+
+class CourseReferentPolicy < ReferentPolicy
+end
+
+class TimeReferentPolicy < ReferentPolicy
+end
 
 
