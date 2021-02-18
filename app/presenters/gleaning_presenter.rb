a0dcd70361c4946639cd582604628cbeac6990f8
Steve Upstill <steve@upstill.net>
2021-02-17 16:58:47 -0800

    Building page content automagically in RecipePresenter

--- app/presenters/gleaning_presenter.rb
+++ app/presenters/gleaning_presenter.rb
@@ -1,10 +1,18 @@
 class GleaningPresenter < BasePresenter
-  def html_content 
+
+  def content_preface
+    recipe_content_buttons(@object) + content_suggestion
+  end
+  
+  def html_content
     @object.content.html_safe if @object.content.present?
   end
 
   def content_suggestion
-    "This is the material gleaned directly from the page. Click #{edit_trimmers_button @object, 'here'} to capture by CSS.".html_safe
+    (@object.content.present? ?
+        'This is the content that has been extracted from the page.' :
+        'Nothing has been extracted from the page.').html_safe + 
+    " To specify how to extract content using CSS, click #{edit_trimmers_button @object, 'here'}.".html_safe
   end
 
   # When a Gleaning is updated, what types of item get replaced?
