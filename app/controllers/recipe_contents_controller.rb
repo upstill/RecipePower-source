d3cb03df53811e44bcf9b6316f84067d5348c2ca
Steve Upstill <steve@upstill.net>
2021-03-05 18:27:07 -0800

    Now handling Cancel in Recipe Content dialogs; misc. styling of dialog elements

--- app/controllers/recipe_contents_controller.rb
+++ app/controllers/recipe_contents_controller.rb
@@ -37,7 +37,6 @@ class RecipeContentsController < ApplicationController
     redirect_to :show unless rcparams = params[:recipe][:recipeContents]
     @content = rcparams[:content]
     tag if rcparams[:token] == 'tag'
-    # redirect_to tag_recipe_contents_path( recipe: { recipeContents: { content: @content } } ) if rcparams[:token] == 'tag'
     ps = ParserServices.new entity: @recipe, content: @content
     # This is a two-, possibly three-phase process:
     # 1) a selection from the browser directs attention to a range of text, which generates a CSS path for an element to parse
@@ -45,7 +44,7 @@ class RecipeContentsController < ApplicationController
     #    for the user to decide how to handle the tag;
     # 3) the user says to enter the problematic tag directly into the dictionary, or specifies an existing tag for what
     #    was meant by the problematic tag. The latter can be optionally entered as a synonym of the intended name.
-    if rcparams[:anchor_path] # Initial annotation on browser selection
+    if rcparams[:anchor_path].present? # Initial annotation on browser selection
       logger.debug "Annotating with anchor_path = '#{rcparams[:anchor_path]}'"
       @annotation, @parse_path = ps.annotate_selection *rcparams.values_at(:token, :anchor_path, :anchor_offset, :focus_path, :focus_offset)
     elsif @parse_path = rcparams[:parse_path] # Specifying an element of the DOM
@@ -65,27 +64,29 @@ class RecipeContentsController < ApplicationController
         @tagtype = rcparams[:tagtype]
         @annotation = rcparams[:content]
         # @tagname of type @tagtype is a mystery tag previously identified by a failed parse.
-        logger.debug "Use tag '#{@tagname}' of type '#{@tagtype}' at path '#{@parse_path}'"
-        # The logic here is as follows, for sorting out words.
-        # -- '@tagname' is the questionable tag
-        # -- 'rcparams[:replacement]' is the id of a tag selected by the user to serve in its place
-        # -- 'rcparams[:assert]' is set if that tag will be added to the dictionary
-        # If there's no replacement tag, the term is just accepted into the dictionary as a new instance of the tagtype.
-        # If there IS a non-nil replacement, that's what we understand the term to mean. If, then, assert is set,
-        # the questionable term will get added to the dictionary as a synonym for the replacement. If not, the alias
-        # is accepted and added to the Seeker as such.
-        # No tagname has been parsed out; proceed to parse the entity denoted by the parse path
-        if (replacement = rcparams[:replacement]).present?
-          old_tag = Tag.find_by id: replacement
-          if rcparams[:assert]
-            new_tag = Tag.assert @tagname, @tagtype
-            new_tag.absorb old_tag, false
+        if params[:button_name] != 'Cancel'
+          logger.debug "Use tag '#{@tagname}' of type '#{@tagtype}' at path '#{@parse_path}'"
+          # The logic here is as follows, for sorting out words.
+          # -- '@tagname' is the questionable tag
+          # -- 'rcparams[:replacement]' is the id of a tag selected by the user to serve in its place
+          # -- 'rcparams[:assert]' is set if that tag will be added to the dictionary
+          # If there's no replacement tag, the term is just accepted into the dictionary as a new instance of the tagtype.
+          # If there IS a non-nil replacement, that's what we understand the term to mean. If, then, assert is set,
+          # the questionable term will get added to the dictionary as a synonym for the replacement. If not, the alias
+          # is accepted and added to the Seeker as such.
+          # No tagname has been parsed out; proceed to parse the entity denoted by the parse path
+          if (replacement = rcparams[:replacement]).present?
+            old_tag = Tag.find_by id: replacement
+            if rcparams[:assert]
+              new_tag = Tag.assert @tagname, @tagtype
+              new_tag.absorb old_tag, false
+            end
+            value = old_tag.name
+          else
+            # No replacement provided => simply assert the tag into the dictionary
+            value = @tagname
+            Tag.assert @tagname, @tagtype
           end
-          value = old_tag.name
-        else
-          # No replacement provided => simply assert the tag into the dictionary
-          value = @tagname
-          Tag.assert @tagname, @tagtype
         end
         @tagname = nil # Go back to the annotation dialog
         @parse_path = nil
